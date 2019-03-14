f3d206dc33c9a4a10de095d645bff6e4e50c029c
Hold The Door~ <37676951+Hodor2333@users.noreply.github.com>
2019-03-14 10:43:35 +0800

    Update EurekaServiceApplication.java

--- src/main/java/com/wangsu/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/com/wangsu/eurekaservice/EurekaServiceApplication.java
@@ -3,7 +3,6 @@ package com.wangsu.eurekaservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-@EnableDiscoveryClient
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
